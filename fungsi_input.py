838f2075cfa512ce50638f6bbb35705bb224b2ba
gepeemwe <86011222+gepeemwe@users.noreply.github.com>
2023-04-14 02:11:53 +0000

    contoh penggunaan variabel "nama" & fungsi "input"

---
+++ fungsi_input.py
@@ -0,0 +1,10 @@
+#berikut adalah contoh penggunaan variabel dan fungsi input
+# contoh penggunaan variabel "nama" secara langsung 
+
+nama = "Makoto"
+print ("nama saya adalah " , nama)
+
+# contoh penggunaan variabel "nama" dengan menggunakan fungsi "input"
+
+nama = input("isikan nama kamu :")
+print ("nama saya adalah " , nama)
