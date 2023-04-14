26c4d38df12c0a4d0019e8dbf30832101b444d5c
gepeemwe <86011222+gepeemwe@users.noreply.github.com>
2023-04-14 03:21:12 +0000

    membuat fugnsi hitung tarif berdasar umum

--- fungsi_baru.py
+++ fungsi_baru.py
@@ -4,4 +4,11 @@ def greeting ():
     nama = input ("Silahkan Masukkan nama Anda : ")
     print ("Selamat datang", nama)
     print ("Mari Mencoba menulis Kode")
+#membuat fungsi "hitungtarif"
+def hitungtarif (tarif):
+    usia = int(input ("silahkan masukan Usia anda: "))
+    jumlahbayar = usia * tarif
+    print ("Tarif masuk anda adalah sebesar:", jumlahbayar)
+
 greeting ()
+hitungtarif(4000)
