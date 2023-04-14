63485adc406d8f538690ec3e2a6adb542539903c
gepeemwe <86011222+gepeemwe@users.noreply.github.com>
2023-04-14 03:02:57 +0000

    contoh membuat fungsi "greeting"

---
+++ fungsi_baru.py
@@ -0,0 +1,7 @@
+#contoh membuat fungsi baru "greeting"
+
+def greeting ():
+    nama = input ("Silahkan Masukkan nama Anda : ")
+    print ("Selamat datang", nama)
+    print ("Mari Mencoba menulis Kode")
+greeting ()
