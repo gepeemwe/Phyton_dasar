45852480ac01c8547c152d176babf68f3749149d
gepeemwe <86011222+gepeemwe@users.noreply.github.com>
2023-04-14 01:19:54 +0000

    contoh penggunaan escape charakter

---
+++ text_paragraf.py
@@ -0,0 +1,13 @@
+#contoh menuliskan text dan paragraf dengan menggunakan escape charakter
+#menggunakan escape carakter "\" untuk membuat tanda petik satu "\n" untuk membuat garis baru 
+print ("Identitas Pelanggan \n Nama yang berbaju merah\'s adalah Yanti " )
+
+#menggunakan escape carakter kutip 3 ''' untuk membuat paragraf sesuai dengan yang diketik
+
+print(
+    '''
+    Ini adalah sebuat text yang memanfaatkan escape caracter
+    membuat text berbentuk paragraf 
+    mari kita coba !!!
+    '''
+)
