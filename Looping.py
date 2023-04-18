6905aa20f3ec1b94b83dae8c19108638173e415a
gepeemwe <86011222+gepeemwe@users.noreply.github.com>
2023-04-18 10:20:45 +0000

    tambah loop dengan for dalam range

--- Looping.py
+++ Looping.py
@@ -7,3 +7,9 @@ a = 0
 while a < 10:
     a = a +1
     print (a)
+
+#Perulangan dengan menggunakan for untuk range
+
+satusampaisepuluh = range (1,11)
+for count in satusampaisepuluh:
+    print (count)
---
+++ fungsi_if.py
@@ -0,0 +1,9 @@
+#Perulangan dengan menggunakan "for"
+for i in range(1, 20):
+    print(i)
+
+#Perulangan menggunakan "while"
+a = 0 
+while a < 10:
+    a = a +1
+    print (a)
